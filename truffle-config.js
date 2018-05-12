25a9f4d6608edea01e381c19ce5252648483dcce
Khizar <khizar.ali.manzoor@gmail.com>
2018-05-12 20:44:41 +0500

    Set custom contract build directory

--- truffle-config.js
+++ truffle-config.js
@@ -2,8 +2,12 @@
 require('babel-register')
 // babel-polyfill as we need it to use async/await
 require('babel-polyfill')
+var path = require('path');
 
 module.exports = {
+
+  contracts_build_directory: path.join(__dirname, './src/assets/data'),
+
   networks: {
     development: {
       host: 'localhost',
@@ -12,12 +16,12 @@ module.exports = {
       gas: 6000000 // Gas limit used for deploys
     },
 
- 	rinkeby: {
-	  host: "localhost", // Connect to geth on the specified
-	  port: 8545,
-	  network_id: '*',
-	  from: "0x955165032303993ecc8b29b02ad1591833b55c4b", // default address to use for any transaction Truffle makes during migrations
-	  gas: 470000 // Gas limit used for deploys
-	}
+   	rinkeby: {
+  	  host: "localhost", // Connect to geth on the specified
+  	  port: 8545,
+  	  network_id: '*',
+  	  from: "0x955165032303993ecc8b29b02ad1591833b55c4b", // default address to use for any transaction Truffle makes during migrations
+  	  gas: 470000 // Gas limit used for deploys
+  	}
   }
 }
